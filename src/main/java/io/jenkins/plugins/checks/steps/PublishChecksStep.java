61c0c048d2b7b118514b706c2bd95e774457f5ac
Kezhi Xiong <august.xkz@gmail.com>
2020-08-07 17:42:19 +0800

    Adjust constructor with required fields

--- src/main/java/io/jenkins/plugins/checks/steps/PublishChecksStep.java
+++ src/main/java/io/jenkins/plugins/checks/steps/PublishChecksStep.java
@@ -6,6 +6,7 @@ import hudson.Extension;
 import hudson.model.Run;
 import hudson.model.TaskListener;
 import io.jenkins.plugins.checks.api.*;
+import org.apache.commons.lang3.StringUtils;
 import org.jenkinsci.plugins.workflow.steps.*;
 import org.kohsuke.stapler.DataBoundConstructor;
 import org.kohsuke.stapler.DataBoundSetter;
@@ -23,53 +24,34 @@ import java.util.*;
 public class PublishChecksStep extends Step implements Serializable {
     private static final long serialVersionUID = 1L;
 
-    private String name;
-    private String detailsURL;
-    private ChecksStatus status;
-    private ChecksConclusion conclusion;
+    private final String name;
+    private final String summary;
+
+    private String title;
+
+    private String text = StringUtils.EMPTY;
+    private String detailsURL = StringUtils.EMPTY;
+    private ChecksStatus status = ChecksStatus.COMPLETED;
+    private ChecksConclusion conclusion = ChecksConclusion.SUCCESS;
     private LocalDateTime startedAt = LocalDateTime.now();
     private LocalDateTime completedAt = LocalDateTime.now();
 
-    private String title;
-    private String summary;
-    private String text;
-
     /**
-     * Empty constructor used by stapler to support pipeline.
+     * Constructor used in pipeline with required fields.
+     *
+     * @param name
+     *         the name of the check
+     * @param summary
+     *         the summary of build
      */
     @DataBoundConstructor
-    public PublishChecksStep() {
+    public PublishChecksStep(final String name, final String summary) {
         super();
-    }
 
-    @DataBoundSetter
-    public void setName(final String name) {
         this.name = name;
-    }
+        this.summary = summary;
 
-    @DataBoundSetter
-    public void setStatus(final ChecksStatus status) {
-        this.status = status;
-    }
-
-    @DataBoundSetter
-    public void setDetailsURL(final String detailsURL) {
-        this.detailsURL = detailsURL;
-    }
-
-    @DataBoundSetter
-    public void setStartedAt(final String startedAt) {
-        this.startedAt = LocalDateTime.parse(startedAt);
-    }
-
-    @DataBoundSetter
-    public void setConclusion(final ChecksConclusion conclusion) {
-        this.conclusion = conclusion;
-    }
-
-    @DataBoundSetter
-    public void setCompletedAt(final String completedAt) {
-        this.completedAt = LocalDateTime.parse(completedAt);
+        this.title = name;
     }
 
     @DataBoundSetter
@@ -78,54 +60,38 @@ public class PublishChecksStep extends Step implements Serializable {
     }
 
     @DataBoundSetter
-    public void setSummary(final String summary) {
-        this.summary = summary;
+    public void setText(final String text) {
+        this.text = text;
     }
 
     @DataBoundSetter
-    public void setText(final String text) {
-        this.text = text;
+    public void setDetailsURL(final String detailsURL) {
+        this.detailsURL = detailsURL;
+    }
+
+    @DataBoundSetter
+    public void setStatus(final ChecksStatus status) {
+        this.status = status;
+    }
+
+    @DataBoundSetter
+    public void setConclusion(final ChecksConclusion conclusion) {
+        this.conclusion = conclusion;
+    }
+
+    @DataBoundSetter
+    public void setStartedAt(final String startedAt) {
+        this.startedAt = LocalDateTime.parse(startedAt);
+    }
+
+    @DataBoundSetter
+    public void setCompletedAt(final String completedAt) {
+        this.completedAt = LocalDateTime.parse(completedAt);
     }
 
     @Override
     public StepExecution start(final StepContext stepContext) {
         return new PublishChecksStepExecution(stepContext, this);
-    }
-
-    /**
-     * This step's execution to actually publish checks.
-     */
-    static class PublishChecksStepExecution extends SynchronousNonBlockingStepExecution<Void> {
-        private static final long serialVersionUID = 1L;
-        private final PublishChecksStep step;
-
-        PublishChecksStepExecution(final StepContext context, final PublishChecksStep step) {
-            super(context);
-            this.step = step;
-        }
-
-        @Override
-        protected Void run() throws Exception {
-            ChecksDetails details = new ChecksDetails.ChecksDetailsBuilder()
-                    .withName(step.name)
-                    .withDetailsURL(step.detailsURL)
-                    .withStatus(step.status)
-                    .withConclusion(step.conclusion)
-                    .withStartedAt(step.startedAt)
-                    .withCompletedAt(step.completedAt)
-                    .withOutput(new ChecksOutput.ChecksOutputBuilder()
-                            .withText(step.title)
-                            .withSummary(step.summary)
-                            .withTitle(step.text)
-                            .build())
-                    .build();
-
-            ChecksPublisher publisher
-                    = ChecksPublisherFactory.fromRun(getContext().get(Run.class), getContext().get(TaskListener.class));
-            publisher.publish(details);
-
-            return null;
-        }
     }
 
     /**
@@ -147,6 +113,43 @@ public class PublishChecksStep extends Step implements Serializable {
         @Override
         public String getDisplayName() { // it's pipeline step, so where does the name shown?
             return "Publish customized checks to SCM platforms";
+        }
+    }
+
+    /**
+     * This step's execution to actually publish checks.
+     */
+    static class PublishChecksStepExecution extends SynchronousNonBlockingStepExecution<Void> {
+        private static final long serialVersionUID = 1L;
+        private final PublishChecksStep step;
+
+        PublishChecksStepExecution(final StepContext context, final PublishChecksStep step) {
+            super(context);
+            this.step = step;
+        }
+
+        @Override
+        protected Void run() throws Exception {
+            ChecksDetails.ChecksDetailsBuilder builder = new ChecksDetails.ChecksDetailsBuilder()
+                    .withName(step.name)
+                    .withStatus(step.status)
+                    .withConclusion(step.conclusion)
+                    .withStartedAt(step.startedAt)
+                    .withCompletedAt(step.completedAt)
+                    .withOutput(new ChecksOutput.ChecksOutputBuilder()
+                            .withTitle(step.title)
+                            .withSummary(step.summary)
+                            .withText(step.text)
+                            .build());
+
+            if (StringUtils.isNotBlank(step.detailsURL)) {
+                builder.withDetailsURL(step.detailsURL);
+            }
+
+            ChecksPublisherFactory.fromRun(getContext().get(Run.class), getContext().get(TaskListener.class))
+                    .publish(builder.build());
+
+            return null;
         }
     }
 }
