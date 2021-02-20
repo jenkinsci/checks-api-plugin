88f492a0dcf2e0f6c0027051eb2f43701a478b5b
Kezhi Xiong <august.xkz@gmail.com>
2021-02-20 11:45:01 +0800

    Rebase master and improve test

--- src/test/java/io/jenkins/plugins/checks/steps/PublishChecksStepTest.java
+++ src/test/java/io/jenkins/plugins/checks/steps/PublishChecksStepTest.java
@@ -11,24 +11,15 @@ import org.junit.jupiter.api.Test;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.Objects;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static io.jenkins.plugins.checks.assertions.Assertions.assertThat;
 import static org.mockito.Mockito.*;
 
 class PublishChecksStepTest {
-
-    StepContext getStepContext() throws IOException, InterruptedException {
-        StepContext context = mock(StepContext.class);
-        when(context.get(Run.class)).thenReturn(mock(Run.class));
-        when(context.get(TaskListener.class)).thenReturn(TaskListener.NULL);
-        return context;
-    }
-
     @Test
     void shouldPublishCheckWithDefaultValues() throws IOException, InterruptedException {
-        StepExecution execution = new PublishChecksStep().start(getStepContext());
+        StepExecution execution = new PublishChecksStep().start(createStepContext());
         assertThat(execution).isInstanceOf(PublishChecksStep.PublishChecksStepExecution.class);
         assertThat(((PublishChecksStep.PublishChecksStepExecution)execution).extractChecksDetails())
                 .usingRecursiveComparison()
@@ -48,10 +39,10 @@ class PublishChecksStepTest {
 
     @Test
     void shouldPublishCheckWithStatusInProgress() throws IOException, InterruptedException {
-        PublishChecksStep step = getModifiedPublishChecksStepObject("an in progress build",
-                ChecksStatus.IN_PROGRESS, null);
+        PublishChecksStep step = createPublishChecksStep("an in progress build", ChecksStatus.IN_PROGRESS,
+                ChecksConclusion.NONE);
 
-        StepExecution execution = step.start(getStepContext());
+        StepExecution execution = step.start(createStepContext());
         assertThat(execution).isInstanceOf(PublishChecksStep.PublishChecksStepExecution.class);
         assertThat(((PublishChecksStep.PublishChecksStepExecution)execution).extractChecksDetails())
                 .usingRecursiveComparison()
@@ -69,11 +60,11 @@ class PublishChecksStepTest {
     }
 
     @Test
-    void shouldPublishCheckWithStatusQueue() throws IOException, InterruptedException {
-        PublishChecksStep step = getModifiedPublishChecksStepObject("a queued build",
-                ChecksStatus.QUEUED, null);
+    void shouldPublishCheckWithStatusQueued() throws IOException, InterruptedException {
+        PublishChecksStep step = createPublishChecksStep("a queued build", ChecksStatus.QUEUED,
+                ChecksConclusion.NONE);
 
-        StepExecution execution = step.start(getStepContext());
+        StepExecution execution = step.start(createStepContext());
         assertThat(execution).isInstanceOf(PublishChecksStep.PublishChecksStepExecution.class);
         assertThat(((PublishChecksStep.PublishChecksStepExecution)execution).extractChecksDetails())
                 .usingRecursiveComparison()
@@ -92,21 +83,20 @@ class PublishChecksStepTest {
 
     @Test
     void shouldPublishCheckWithSetValues() throws IOException, InterruptedException {
-        PublishChecksStep step = new PublishChecksStep();
-        step.setName("Jenkins");
-        step.setSummary("a check made by Jenkins");
-        step.setTitle("Jenkins Build");
-        step.setText("a failed build");
-        step.setStatus(ChecksStatus.IN_PROGRESS);
-        step.setConclusion(ChecksConclusion.FAILURE);
-        step.setDetailsURL("http://ci.jenkins.io");
+        PublishChecksStep step = createPublishChecksStep("a failed build", ChecksStatus.IN_PROGRESS,
+                ChecksConclusion.FAILURE);
+
         step.setActions(Arrays.asList(
                 new PublishChecksStep.StepChecksAction("label-1", "description-1", "identifier-1"),
                 new PublishChecksStep.StepChecksAction("label-2", "description-2", "identifier-2")));
-        PublishChecksStep step = getModifiedPublishChecksStepObject("a failed build",
-                ChecksStatus.IN_PROGRESS, ChecksConclusion.FAILURE);
+        assertThat(step.getActions().stream().map(PublishChecksStep.StepChecksAction::getLabel))
+                .containsExactlyInAnyOrder("label-1", "label-2");
+        assertThat(step.getActions().stream().map(PublishChecksStep.StepChecksAction::getDescription))
+                .containsExactlyInAnyOrder("description-1", "description-2");
+        assertThat(step.getActions().stream().map(PublishChecksStep.StepChecksAction::getIdentifier))
+                .containsExactlyInAnyOrder("identifier-1", "identifier-2");
 
-        StepExecution execution = step.start(getStepContext());
+        StepExecution execution = step.start(createStepContext());
         assertThat(execution).isInstanceOf(PublishChecksStep.PublishChecksStepExecution.class);
         assertThat(((PublishChecksStep.PublishChecksStepExecution)execution).extractChecksDetails())
                 .usingRecursiveComparison()
@@ -134,20 +124,23 @@ class PublishChecksStepTest {
         assertThat(descriptor.getRequiredContext().toArray()).containsExactlyInAnyOrder(Run.class, TaskListener.class);
     }
 
-    private PublishChecksStep getModifiedPublishChecksStepObject(final String stepText, final ChecksStatus status,
-                                                                 final ChecksConclusion conclusion) {
+    private StepContext createStepContext() throws IOException, InterruptedException {
+        StepContext context = mock(StepContext.class);
+        when(context.get(Run.class)).thenReturn(mock(Run.class));
+        when(context.get(TaskListener.class)).thenReturn(TaskListener.NULL);
+        return context;
+    }
+
+    private PublishChecksStep createPublishChecksStep(final String stepText, final ChecksStatus status,
+                                                      final ChecksConclusion conclusion) {
         PublishChecksStep step = new PublishChecksStep();
         step.setName("Jenkins");
         step.setSummary("a check made by Jenkins");
         step.setTitle("Jenkins Build");
-        step.setText(stepText);
-        if (Objects.nonNull(status)) {
-            step.setStatus(status);
-        }
-        if (Objects.nonNull(conclusion)) {
-            step.setConclusion(conclusion);
-        }
         step.setDetailsURL("http://ci.jenkins.io");
+        step.setText(stepText);
+        step.setStatus(status);
+        step.setConclusion(conclusion);
 
         return step;
     }
