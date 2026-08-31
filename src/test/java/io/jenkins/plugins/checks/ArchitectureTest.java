e715e64819b9abf31a06898afbfd1664f24c9b02
Tim Jacomb <timjacomb1@gmail.com>
2026-08-31 15:17:30 +0100

    Update Jenkins baseline to 2.555.3 and require Java 21

--- src/test/java/io/jenkins/plugins/checks/ArchitectureTest.java
+++ src/test/java/io/jenkins/plugins/checks/ArchitectureTest.java
@@ -5,7 +5,7 @@ import com.tngtech.archunit.junit.AnalyzeClasses;
 import com.tngtech.archunit.junit.ArchTest;
 import com.tngtech.archunit.lang.ArchRule;
 import com.tngtech.archunit.lang.syntax.ArchRuleDefinition;
-import edu.hm.hafner.util.ArchitectureRules;
+import edu.hm.hafner.archunit.ArchitectureRules;
 import io.jenkins.plugins.util.PluginArchitectureRules;
 
 /**
